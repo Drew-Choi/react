c8a8f5727874bb702af9b6c23d9f0487d3e49681
Drew <drc920630@gmail.com>
2023-03-13 23:24:40 +0900

    리액트 버튼 만들기 성공

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+// 워닝 없애는 코드
+
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
-  let post = '강남 우동 맛집';
+  //선언부
+  let post = "역삼 우동 맛집";
+  let [title] = useState(["여자 코트 추천", "남성 코트 추천", "여성 원피스"]);
+  let [like, likeFunc] = useState(0);
+  let [logo] = useState("React Blog");
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>{logo}</h4>
       </div>
-      <h4 className="food" style={{color : "red", fontSize : "50px"}}> { post } </h4>
+      <div className="list">
+        <h4>
+          {title[0]}
+          <span
+            className="like"
+            onClick={() => {
+              likeFunc(like + 1);
+            }}
+          >
+            좋아요
+          </span>
+          {like}
+        </h4>
+        <p>3월 13일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>3월 13일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>3월 13일 발행</p>
+      </div>
+      <h4 className="food" style={{ color: "red", fontSize: "50px" }}>
+        {post}
+      </h4>
     </div>
   );
 }
